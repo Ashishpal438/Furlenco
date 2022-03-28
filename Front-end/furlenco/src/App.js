c092236b22a110a7f63c00e282506ddf93183be6
Ashish kumar Pal <ashishpal438@gmail.com>
2022-03-29 01:34:06 +0530

    Showcase section completed

--- Front-end/furlenco/src/App.js
+++ Front-end/furlenco/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import { Navbar } from './components/Navbar/Navbar';
-import {Home} from './components/Home/Home.jsx'
+import "./App.css";
+import { Navbar } from "./components/Home/Navbar/Navbar";
+import { Home } from "./components/Home/Home.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-         <Navbar/>
-         <Home/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
