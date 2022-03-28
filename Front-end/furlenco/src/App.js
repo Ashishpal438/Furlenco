79f7e81e34cd2cb33da44371ab54ac4826a2c015
Ashish kumar Pal <ashishpal438@gmail.com>
2022-03-29 02:04:45 +0530

    revert 3

--- Front-end/furlenco/src/App.js
+++ Front-end/furlenco/src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import { Navbar } from "./components/Home/Navbar/Navbar";
-import { Home } from "./components/Home/Home.jsx";
-import { Route, Routes } from "react-router-dom";
-
-import Bottom from './compotent/electronics/Bottom';
-import Electronics from './compotent/electronics/Electronics';
-import Footer from './compotent/electronics/Footer';
-import Login from './compotent/Login/Login';
-import SignUp from './compotent/Login/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/electronics" element={<Electronics />} />
-      </Routes>
-      {/* <Login />
-      <SignUp/> */}
-      {/* <Electronics /> */}
-      <Bottom/>
-      <Footer/>
-=======
+
+    </div>
+  );
+}
+
+export default App;
