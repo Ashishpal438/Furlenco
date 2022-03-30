8a5f23b44d2a5833e3dbfd9212ab0a78fa0c9c68
Ashish kumar Pal <ashishpal438@gmail.com>
2022-03-30 12:38:39 +0530

    Products and Collections updated

--- Front-end/furlenco/src/components/Home/Home.jsx
+++ Front-end/furlenco/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Showcase } from './Showcase/Showcase'
 import styles from './Home.module.css'
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import { useContext } from 'react'
 import { DataContext } from '../../Contexts/DataContext'
 import { BuildYourOwn } from './BuildYourOwn'
@@ -9,56 +9,50 @@ import { BuildYourOwn } from './BuildYourOwn'
 export const Home = () => {
 
 
-  const {products,collections} = useContext(DataContext)
-  const [data,setData] =  useState([])
+  const { products, collections } = useContext(DataContext)
+  const [data, setData] = useState([...products])
 
-  useEffect(() => {
-    setData(products)
-  }, [])
-
-
-
-  const handleData = (query,index) => {
+  const handleData = (query, index) => {
     const allActiveClasses = document.querySelectorAll('.active')
     console.log(allActiveClasses);
-    for(let i=0;i<allActiveClasses.length;i++){
+    for (let i = 0; i < allActiveClasses.length; i++) {
       allActiveClasses[i].style = 'border-bottom: 2px solid #fff'
     }
     allActiveClasses[index].style = 'border-bottom: 2px solid #FFCD32'
 
-    if(query === null){
+    if (query === null) {
       setData([])
     }
-    else{
+    else {
       setData(query)
     }
   }
 
   return (
     <>
-      <Showcase/>
+      <Showcase />
       <div className={styles.productContainer}>
-          <div onClick={() => handleData(products,0)}  className='active'>PRODUCTS</div>
-          <div onClick={() => handleData(collections,1)}  className='active' >COLLECTIONS</div>
-          <div onClick={() => handleData(null,2)}  className='active'> BUILD YOUR OWN</div>
+        <div onClick={() => handleData(products, 0)} className='active'>PRODUCTS</div>
+        <div onClick={() => handleData(collections, 1)} className='active' >COLLECTIONS</div>
+        <div onClick={() => handleData(null, 2)} className='active'> BUILD YOUR OWN</div>
       </div>
-     
-     {/* display products in a single row */}
+
+      {/* display products in a single row */}
       <div className={styles.productRow}>
         {
-          data.length=== 0 ? <div> <BuildYourOwn/> </div> :
-          data.map(product => {
-            return (  
-              <div className={styles.product}>
-                <img src={product.image} alt="product"/>
-                <div className={styles.productTitle}>{product.title}</div>
-                {/* display new launch or not */}
-                {
-                  product.newlaunch ? <div className={styles.newLaunch}>NEW LAUNCH</div> : null
-                }
-              </div>
-            ) 
-          })
+          data.length === 0 ? <div> <BuildYourOwn /> </div> :
+            data.map(product => {
+              return (
+                <div className={styles.product}>
+                  <img src={product.image} alt="product" />
+                  <div className={styles.productTitle}>{product.title}</div>
+                  {/* display new launch or not */}
+                  {
+                    product.newlaunch ? <div className={styles.newLaunch}>NEW LAUNCH</div> : null
+                  }
+                </div>
+              )
+            })
         }
       </div>
     </>
