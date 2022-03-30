65716caafdb18717745a23aa109829ae18cf6665
Ashish kumar Pal <ashishpal438@gmail.com>
2022-03-30 12:35:13 +0530

    Products and collections completed

--- Front-end/furlenco/src/index.js
+++ Front-end/furlenco/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {DataContextProvider} from './Contexts/DataContext';
 
 ReactDOM.render(
   <React.StrictMode>
+    <DataContextProvider>
     <App />
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
